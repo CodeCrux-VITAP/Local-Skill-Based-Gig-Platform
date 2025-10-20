576a2a204b397dea868787fe0afaa35a7615f647
Roshan <roshankandregula07@gmail.com>
2025-10-20 14:02:02 +0530

    Hosted local PostgreSQL via Supabase

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const db = new Client({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 db.connect()
